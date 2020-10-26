b7112de3b36d965e3594d4efc52d893a242b2e23
Olha NEKRASHEVYCH <olha.idf@gmail.com>
2020-10-26 20:29:56 +0100

    formulaire du menu(EntityType)

--- src/Controller/AdminMenusController.php
+++ src/Controller/AdminMenusController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Menus;
+use App\Form\MenuType;
 use App\Repository\MenusRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -19,4 +22,84 @@ class AdminMenusController extends AbstractController
             'menus' => $menus,
         ]);
 }
+/**
+     * @Route("/admin/menus/create", name="menu_create")
+     */
+    public function createMenu(Request $request){
+        $menu = new Menus();
+        $form = $this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            
+            if($form->isValid()){
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($menu);
+                $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'Le menu a bien été modifiée'
+                );
+            }
+            else{
+                $this->addFlash(
+                    'danger',
+                    'Une erreur est survenue'
+                );
+            }
+                return $this->redirectToRoute('admin_menus');
+            }
+        
+return $this->render('admin/adminMenusForm.html.twig', [
+            'formulaireMenu' => $form->createView()
+        ]);
+        
+    }
+
+    /**
+     * @Route("/admin/menus/update-{id}", name="menu_update")
+     */
+    public function updateMenu(MenusRepository $menusRepository, $id, Request $request)
+    {
+        $menu = $menusRepository->find($id);
+        $form = $this->createForm(MenuType::class, $menu);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+                $manager = $this->getDoctrine()->getManager();
+                $manager->persist($rmenu);
+                $manager->flush();
+                $this->addFlash(
+                    'success',
+                    'Le menu a bien été modifiée'
+                );
+                
+                return $this->redirectToRoute('admin_menus');
+            }
+            return $this->render('admin/adminMenusForm.html.twig', [
+                'formulaireMenu' => $form->createView()
+            ]);
+    }
+
+    /**
+     * @Route("/admin/menus/delete-{id}", name="menu_delete")
+     */
+    public function deleteMenu(MenusRepository $menusRepository, $id)
+    {
+        $menu= $menusRepository->find($id);
+        
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($menu);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            'Le menu a bien été supprimée'
+        );
+
+       
+
+        return $this->redirectToRoute('admin_menus');
+    }
+
 }
---
+++ src/Form/MenuType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Menus;
+use App\Entity\Restaurants;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+class MenuType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'label' => 'Nom du menu',
+                'attr' => [
+                    'placeholder' => 'menu1'
+                ]
+            ]
+            )
+            ->add('composants', TextType::class, [
+                'required' => true,
+                'label' => 'Composants',
+                'attr' => [
+                    'placeholder' => 'Liste de composants'
+                ]
+            ]
+            )
+            ->add('img_menu', TextType::class, [
+                'required' => true,
+                'label' => 'Photo du menu',
+                'attr' => [
+                    'placeholder' => 'image.jpg'
+                ]
+            ]
+            )
+            ->add('prix',  MoneyType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'ex.: 99,00',
+                    'min' => 0
+                ]
+            ])
+            ->add('restaurant', EntityType::class, array(
+                'class'=>Restaurants::class,
+                'choice_label'=>'id',
+            )
+                
+            )
+            ->add('save', SubmitType::class, [
+                'label' => 'Valider'
+            ]
+            )
+        ;
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Menus::class,
+        ]);
+    }
+}
